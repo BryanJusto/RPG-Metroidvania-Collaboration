52d3dc58d5354500928b39ec23cce05a479db2c2
BryanJusto <tsarbelyyvolk@gmail.com>
2019-10-07 05:01:37 -0400

    > RayCasting now works
    > Ray casting now works
    > Still no real AI <

--- Assets/Scripts/enemyAi.cs
+++ Assets/Scripts/enemyAi.cs
@@ -9,14 +9,11 @@ public class enemyAi : MonoBehaviour
     public Transform originPoint;
     public float range;
     public Transform target;
-    private Vector2 dir = new Vector2(1, 0); //because facing left first (?)
     Rigidbody2D rb;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        rb = GetComponent<Rigidbody2D>();
+    void Start(){
+
     }
 
     void Update(){
@@ -24,24 +21,19 @@ public class enemyAi : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        
-//        rb.velocity=new Vector2(speed, rb.velocity.y);
-        Debug.DrawRay(originPoint.position, (dir * range));
-        RaycastHit2D hit = Physics2D.Raycast(originPoint.position, dir, range);
-        if (hit == true)
+    void FixedUpdate(){
+
+
+        LayerMask hu = LayerMask.GetMask("playerLayer");
+        RaycastHit2D hit = Physics2D.Raycast(originPoint.position, Vector2.right, 10, hu);
+        Debug.DrawRay(originPoint.position, (Vector2.right * 10));
+        if (hit)
         {
-            Debug.Log("true");
-            if (hit.collider.CompareTag("ground"))
-            {
-                //flip him and don't forget to flip his raycast as well
-            }
-            if (hit.collider.CompareTag("Player"))
-            {
-                Debug.Log("hit");
-                transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, height), new Vector2(target.position.x, height), speed * Time.deltaTime);
+            Debug.Log("hit");
+            if(Vector3.Distance(transform.position,target.position) > 1){
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
             }
         }
+
     }
 }
